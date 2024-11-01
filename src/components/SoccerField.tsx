035b190344c8a1a07299062ade715ded816d3fa4
JoseRMorales <josera2000@yahoo.es>
2024-11-01 23:51:53 +0100

    Add lineup configurations and update styling functions

--- src/components/SoccerField.tsx
+++ src/components/SoccerField.tsx
@@ -1,37 +1,8 @@
 import PlayerCard from '@/components/PlayerCard'
+import lineups from '@/lib/lineups'
 import React from 'react'
 
-const oneTwoThreeOneLineUp = [
-  { x: 50, y: 75 }, // GK
-  { x: 35, y: 65 }, // Left Defender
-  { x: 65, y: 65 }, // Right Defender
-  { x: 25, y: 50 }, // Left Mid
-  { x: 50, y: 55 }, // Center Mid
-  { x: 75, y: 50 }, // Right Mid
-  { x: 50, y: 40 } // Forward
-]
-
-const oneOneOneThreOneOneLineUp = [
-  { x: 50, y: 95 }, // GK
-  { x: 50, y: 75 }, // Center Defender
-  { x: 25, y: 55 }, // Left Mid
-  { x: 50, y: 55 }, // Center Mid
-  { x: 75, y: 55 }, // Right Mid
-  { x: 50, y: 35 }, // Advanced Mid
-  { x: 50, y: 15 } // Forward
-]
-
-const oneThreeTwoOneLineUp = [
-  { x: 50, y: 90 }, // GK
-  { x: 25, y: 65 }, // Left Defender
-  { x: 50, y: 65 }, // Center Defender
-  { x: 75, y: 65 }, // Right Defender
-  { x: 35, y: 40 }, // Left Mid
-  { x: 65, y: 40 }, // Right Mid
-  { x: 50, y: 15 } // Forward
-]
-
-const positions = oneThreeTwoOneLineUp
+const positions = lineups['2-3-1']
 
 const SoccerField = () => {
   return (
---
+++ src/lib/lineups.ts
@@ -0,0 +1,21 @@
+const lineups = {
+  '2-3-1': [
+    { x: 35, y: 65 }, // Left Defender
+    { x: 65, y: 65 }, // Right Defender
+    { x: 25, y: 50 }, // Left Mid
+    { x: 50, y: 55 }, // Center Mid
+    { x: 75, y: 50 }, // Right Mid
+    { x: 50, y: 40 } // Forward
+  ],
+  '3-2-1': [
+    { x: 50, y: 90 }, // GK
+    { x: 25, y: 65 }, // Left Defender
+    { x: 50, y: 65 }, // Center Defender
+    { x: 75, y: 65 }, // Right Defender
+    { x: 35, y: 40 }, // Left Mid
+    { x: 65, y: 40 }, // Right Mid
+    { x: 50, y: 15 } // Forward
+  ]
+}
+
+export default lineups
